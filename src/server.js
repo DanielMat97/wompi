872eef2a53fdeb545bdb2575af56cb788459d709
Daniel Mateus Ovalle <dmateus17.dm@gmail.com>
2022-11-18 16:54:58 -0500

    feat: config server utils

--- src/server.js
+++ src/server.js
@@ -3,6 +3,17 @@ const app = express();
 
 const { config_server } = require('./application/config/index');
 
+const cors = require('cors');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
 app.listen(config_server.port, () => {
     console.log('Server init on port: ' + config_server.port);
     app.use(`/${config_server.version}/${config_server.name}`, require('./infrastructure/adapters/http/routes'));
